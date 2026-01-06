d248794bd2b97cf62ac9b06d4b40215118d75511
TejasMarthak <tejasmarthak1909@gmail.com>
2026-01-06 21:54:03 +0530

    6/1/2026

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link className="nav-link" aria-current="page" to="/">
                   Home
                 </Link>
                 {/* <a className="nav-link active" aria-current="page" href="#">
@@ -53,6 +53,15 @@ export default function Navbar(props) {
                 Search
               </button>
             </form> */}
+            {/* <div className="d-flex">
+              <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height: "20px", width: "20px", cursor: "pointer"}}></div>
+              <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height: "20px", width: "20px", cursor: "pointer"}}></div>
+              <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height: "20px", width: "20px", cursor: "pointer"}}></div>
+              <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{ height: "20px", width: "20px", cursor: "pointer" }}></div>
+              <div className="bg-dark rounded mx-2" onClick={()=>{props.toggleMode('dark')}} style={{ height: "20px", width: "20px", cursor: "pointer" }}></div>
+              <div className="bg-light rounded mx-2" onClick={()=>{props.toggleMode('light')}} style={{ height: "20px", width: "20px", cursor: "pointer" }}></div>
+            </div> */}
+            
             <div
               className={`form-check form-switch text-${
                 props.mode === "light" ? "dark" : "light"
@@ -60,13 +69,13 @@ export default function Navbar(props) {
             >
               <input
                 className="form-check-input"
-                onClick={props.toggleMode}
+                onClick={()=>{props.toggleMode(null)}}
                 type="checkbox"
                 role="switch"
                 id="switchCheckDefault"
               />
               <label className="form-check-label" for="switchCheckDefault">
-                EnableDarkMode
+                Toggle Mode
               </label>
             </div>
           </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,10 +25,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to clipboard!", "success");
     }
 
@@ -56,7 +53,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} words and {text.length} characters</p>
                 <p>{0.48 * text.split(" ").filter((element) => { return element.length !== 0 }).length} minutes and {0.48 * text.length} Seconds to read this text.</p> {/*time to read text entered in seconds*/}
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Nothing to preview."}</p>
